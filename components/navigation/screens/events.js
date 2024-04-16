85f1b130b631fe1f2c1575c051f4364989c58856
Abhishek18m <145980991+Abhishek18m@users.noreply.github.com>
2024-04-16 17:00:23 +0530

    code optimisation with rb sheet

--- components/navigation/screens/events.js
+++ components/navigation/screens/events.js
@@ -1,12 +1,15 @@
+import React, {useEffect, useRef, useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   Linking,
-  SafeAreaView,
+  Dimensions,
+  Image,
+  TextInput,
+  FlatList,
 } from 'react-native';
-import React, {useEffect, useRef, useState} from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
 import RBSheet from 'react-native-raw-bottom-sheet';
@@ -18,19 +21,24 @@ import BookAppointment from './eventComponents/BookAppointment';
 import {useAppCommonDataProvider} from '../../UseAppCommonDataProvider';
 import {appColors} from '../../../utils/Appcolors';
 import SelectRelation from './eventComponents/SelectRelation';
+import SearchList from './eventComponents/SearchList';
 
-const events = () => {
+const timeOut = 400;
+const Events = () => {
   const refDataEmergency = useRef();
+  const refDataEmergency2 = useRef();
+  const inputRef = useRef();
   const {colorScheme} = useAppCommonDataProvider();
+
+  const [pageIndex, setPageIndex] = useState('UserConfirmation');
+  const [forMyself, setForMyself] = useState(true);
 
   const onSuccess = e => {
     console.log(e, 'eeeeee');
     Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err),
+      console.error('An error occurred', err),
     );
   };
-  const [pageIndex, setPageIndex] = useState('UserConfirmation');
-  const [forMyself, setForMyself] = useState(true);
 
   const onDone = () => {
     refDataEmergency?.current?.close();
@@ -41,7 +49,6 @@ const events = () => {
     switch (pageIndex) {
       case 'UserConfirmation':
         return <UserConfirmation onPress={setPageIndex} onDone={onDone} />;
-
       case 'ForWhom':
         return (
           <ForWhom
@@ -75,90 +82,75 @@ const events = () => {
           <SelectRelation
             onDone={() => setPageIndex('BookAppointment')}
             onSearch={() => {
-              setPageIndex('SearchList'), refDataEmergency?.current?.close();
-              console.log(pageIndex);
+              setPageIndex('SearchList');
+              refDataEmergency?.current?.close();
+              setTimeout(() => {
+                refDataEmergency2?.current?.open();
+              }, timeOut);
             }}
           />
         );
-
       default:
-        <UserConfirmation onPress={e => setPageIndex(e)} />;
+        return <UserConfirmation onPress={e => setPageIndex(e)} />;
     }
   };
-  {
-    console.log(pageIndex);
-  }
+
+  const renderRBSheet = (ref, height, content) => (
+    <RBSheet
+      ref={ref}
+      onClose={() => {
+        pageIndex !== 'SelectRelation' && setPageIndex('UserConfirmation');
+      }}
+      height={height}
+      openDuration={250}
+      customStyles={{
+        container: {
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          backgroundColor:
+            colorScheme === 'light' ? appColors?.white : appColors?.black,
+        },
+      }}>
+      {content}
+    </RBSheet>
+  );
+  const searchListBtn = () => {
+    refDataEmergency2?.current?.close();
+    setTimeout(() => {
+      refDataEmergency?.current?.open();
+      setPageIndex('BookAppointment');
+    }, timeOut);
+  };
   return (
     <View style={styles.container}>
-      {pageIndex == 'SearchList' ? (
-        <View style={{flex: 1, paddingHorizontal: 30, marginTop: 50}}>
-          <View
-            style={{
-              justifyContent: 'space-between',
-              flexDirection: 'row',
-
-              width: '100%',
+      <QRCodeScanner
+        onRead={onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        bottomContent={
+          <TouchableOpacity
+            style={styles.buttonTouchable}
+            onPress={() => {
+              refDataEmergency?.current?.open();
             }}>
-            <Text style={{textAlign: 'left', fontSize: 18, fontWeight: '700'}}>
-              Select Relations
-            </Text>
+            <Text style={styles.buttonText}>done</Text>
+          </TouchableOpacity>
+        }
+      />
 
-            <TouchableOpacity
-              onPress={() => {
-                setPageIndex('BookAppointment'),
-                  refDataEmergency?.current?.open();
-              }}>
-              <Text
-                style={{
-                  color: '#007AFF',
-                  fontSize: 17,
-                  fontWeight: '400',
-                }}>
-                Done
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      ) : (
-        <QRCodeScanner
-          onRead={onSuccess}
-          flashMode={RNCamera.Constants.FlashMode.torch}
-          bottomContent={
-            <TouchableOpacity
-              style={styles.buttonTouchable}
-              onPress={() => {
-                refDataEmergency?.current?.open();
-              }}>
-              <Text style={styles.buttonText}>done</Text>
-            </TouchableOpacity>
-          }
-        />
+      {renderRBSheet(refDataEmergency, 410, eventView())}
+      {renderRBSheet(
+        refDataEmergency2,
+        Dimensions.get('screen').height,
+        <SearchList inputRef={inputRef} onPress={searchListBtn} />,
       )}
-
-      <RBSheet
-        ref={refDataEmergency}
-        onClose={() => {
-          pageIndex != 'SelectRelation' && setPageIndex('UserConfirmation');
-        }}
-        height={410}
-        openDuration={250}
-        customStyles={{
-          container: {
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderTopLeftRadius: 30,
-            borderTopRightRadius: 30,
-            backgroundColor:
-              colorScheme === 'light' ? appColors?.white : appColors?.black,
-          },
-        }}>
-        {eventView()}
-      </RBSheet>
     </View>
   );
 };
 
-export default events;
+export default Events;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
